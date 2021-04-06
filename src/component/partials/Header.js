728f429235a3699f546b7bc5380fa2da011151df
austintip <austin.d.tipograph@gmail.com>
2021-04-05 19:05:38 -0700

    added email address

--- src/component/partials/Header.js
+++ src/component/partials/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header className="App-header">
             <h1 className="header_name">Austin Tipograph</h1>
+            <h5><a className="header_email" href="mailto:austin.d.tipograph@gmail.com">austin.d.tipograph@gmail.com</a></h5>
             <nav>
                 <Link to="/">Home Page</Link>{' | '}
                 <Link to="/about">About Me!</Link>{' | '}
